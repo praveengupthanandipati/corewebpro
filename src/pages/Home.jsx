cb01fb4d6e6811d4f9539cc9a1c128f920724db9
Praveen <praveennandipati@gmail.com>
2025-03-25 11:14:34 +0530

    commit files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import videoSrc from "../assets/videos/video.mp4";
 import HomeAboutColumns from "../components/HomeAboutColumns";
 
-
-const Home = () => {  
+const Home = () => {
+  const bannerTitle = "Crafting Digital Experiences That Inspire Growth";
+  const aboutTitle =
+    " Core Webpro is a team of highly passionate web designers and developers. We do ordinary things extraordinarily well. Our team is driven by the desire to excel - but our determination and commitment to an unrelenting pursuit of your goal enables us to attain the success that we seek.";
   return (
     <>
       <section className="video-background">
@@ -14,9 +16,7 @@ const Home = () => {
       <main>
         <section className="homeMain-title">
           <div className="container">
-            <h1 className="text-text-color font-bold">
-              Crafting Digital Experiences That Inspire Growth
-            </h1>
+            <h1 className="text-text-color font-bold">{bannerTitle}</h1>
             <a className="btn-blue text-white bg-sec d-inline-block" to="">
               <span className="text-uppercase"> Read More</span>
             </a>
@@ -25,15 +25,8 @@ const Home = () => {
 
         <section className="aboutHome">
           <div className="container">
-            <h2 className="text-text-color font-light">
-              Core Webpro is a team of highly passionate web designers and
-              developers. We do ordinary things extraordinarily well. Our team
-              is driven by the desire to excel - but our determination and
-              commitment to an unrelenting pursuit of your goal enables us to
-              attain the success that we seek.
-            </h2>
+            <h2 className="text-text-color font-bold">{aboutTitle}</h2>
             <HomeAboutColumns />
-           
           </div>
         </section>
       </main>
