7c1d9c6083c9ca72a848527c10cf78a1924b160c
Praveen <praveennandipati@gmail.com>
2025-06-19 10:42:19 +0530

    commit base files

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>      
+      <header>
+        <div className='custom-container'>
+             <p>Header</p>
+        </div>
+      </header>     
+    </>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <main>
+        <div className='custom-container'>
+          <h1>Welcome to the Home Page</h1>
+          <p>This is the main content area.</p>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Home
